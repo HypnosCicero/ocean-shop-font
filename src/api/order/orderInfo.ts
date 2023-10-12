06a8f0a1a521d1a59467259dce53b9950c5302b6
HypnosCicero <1345764659@qq.com>
2023-10-12 10:53:33 +0800

    style(orderInfo.ts): changed the styles.

--- src/api/order/orderInfo.ts
+++ src/api/order/orderInfo.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-const api_name = '/admin/order/orderInfo' //TODO 后端路径
+const api_name = '/admin/order/orderInfo'; //TODO 后端路径
 
 export default {
 
@@ -9,14 +9,14 @@ export default {
       url: `${api_name}/${page}/${limit}`,
       method: 'get',
       params: searchObj // url查询字符串或表单键值对
-    })
+    });
   },
 
   show(orderId: argsType) {
     return request({
       url: `${api_name}/get/${orderId}`,
       method: 'get'
-    })
+    });
   },
 
   deliver(orderDeliverVo: JSON) {
@@ -24,20 +24,20 @@ export default {
       url: `${api_name}/deliver`,
       method: 'post',
       data: orderDeliverVo
-    })
+    });
   },
 
   findReceiveList(wareId: argsType, date: Date) {
     return request({
       url: `${api_name}/findReceiveList/${wareId}/${date}`,
       method: 'get'
-    })
+    });
   },
 
   findLeaderReceiveList(leaderId: argsType, date: Date) {
     return request({
       url: `${api_name}/findLeaderReceiveList/${leaderId}/${date}`,
       method: 'get'
-    })
+    });
   }
 }
