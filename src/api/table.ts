bfa652d5a046684f807be4c3440506644ea597f1
HypnosCicero <1345764659@qq.com>
2023-10-13 08:56:49 +0800

    style(api/table.ts): change the style in tables.ts

--- src/api/table.ts
+++ src/api/table.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request';
 
 const api_name = '';//TODO 需要更改api的地址
 
-export function getList(params:JSON) { //TODO 我甚至不知道这个params是什么。
+export function getList(params: JSON) { //TODO 我甚至不知道这个params是什么。
     return request({
         url: `${api_name}`,
         method: 'get',
